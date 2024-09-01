91c8b2026407d9cf54df73514bba966d75ee4e1e
IjazAhmedchirp502 <162543597+IjazAhmedchirp502@users.noreply.github.com>
2024-09-01 21:42:13 +0500

    ff

---
+++ android/app/src/main/kotlin/com/example/provider_favorite_page/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.provider_favorite_page
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/home_page.dart
@@ -0,0 +1,53 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_favorite_page/provider/favorite_provider.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    final words = nouns.take(50).toList();
+    final provider = Provider.of<FavoriteProvider>(context);
+    return Scaffold(
+      body: SafeArea(
+          child: ListView.builder(
+        itemCount: words.length,
+        itemBuilder: (context, index) {
+          final word = words[index];
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              height: 50,
+              width: double.infinity,
+              decoration: const BoxDecoration(color: Colors.grey),
+              child: ListTile(
+                title: Text(word),
+                trailing: IconButton(
+                  onPressed: () {
+                    provider.toggleFavorite(word);
+                  },
+                  icon: provider.isExist(word)
+                      ? Icon(
+                          Icons.favorite_border,
+                          color: Colors.red,
+                        )
+                      : Icon(
+                          Icons.favorite_border,
+                        ),
+                ),
+              ),
+            ),
+          );
+        },
+      )),
+    );
+  }
+}
